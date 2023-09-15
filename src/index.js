8a70b0279d9dddd4c26534c1c9d116ffdd142374
centinela1024tb <centinela1024tb@gmail.com>
2023-09-15 15:56:47 -0600

    [CLEAR] Delete and create files and initial config

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+WebFont.load({
+  google: {
+    // Work+Sans:wght@400;500;700
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+const Index = () => {
+  return (
+    <App />
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Index />);
+// root.render(<React.StrictMode> <Index /> </React.StrictMode> );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
